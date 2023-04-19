785c63cc5e9ef1aefe038037ce2c32bf25165535
hyunseok <prking94@naver.com>
2023-04-19 11:09:38 +0900

    test commit01

--- js/codingTest.js
+++ js/codingTest.js
@@ -1,12 +1,43 @@
-function solution(n) {
-    let answer = 0;
+// function solution(n) {
+//     let answer = 0;
    
-    for(let i=0; i<=n; i+=2){
-        if (i % 2 === 0) {
-            answer += i;
-        }
-    }
-    return answer;
-}
+//     for(let i=0; i<=n; i+=2){
+//         if (i % 2 === 0) {
+//             answer += i;
+//         }
+//     }
+//     return answer;
+// }
 
-document.write(solution(10))
+// document.write(solution(10))
+
+function solution(n, arr1, arr2) {
+    const map1 = arr1.map(binaryString);
+    const map2 = arr2.map(binaryString);
+    
+    const finalMap = [];
+    
+    for (let i = 0; i < n; i++) {
+      let row = '';
+      for (let j = 0; j < n; j++) {
+        if (map1[i][j] === '1' || map2[i][j] === '1') {
+          row += '#';
+        } else {
+          row += ' ';
+        }
+      }
+      finalMap.push(row);
+    }
+    
+    return finalMap;
+  }
+  
+  // 10진수를 2진수 문자열로 변환하는 함수
+  function binaryString(decimal) {
+    let binaryString = decimal.toString(2);
+    // 문자열의 길이가 n보다 작을 경우 앞에 0을 채움
+    while (binaryString.length < n) {
+      binaryString = '0' + binaryString;
+    }
+    return binaryString;
+  }
